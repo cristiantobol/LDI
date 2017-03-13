d1960edd73c6bf9ff844b3c76e3813a70dba999d
dave <dave@armchair.mb.ca>
2017-03-13 12:36:06 +0000

    Add some helpful comments.

--- Silt/src/uk/ac/derby/ldi/silt/transpiler/In.java
+++ Silt/src/uk/ac/derby/ldi/silt/transpiler/In.java
@@ -2,11 +2,14 @@ package uk.ac.derby.ldi.silt.transpiler;
 
 import java.util.regex.Pattern;
 
+/** Indentation tool. */
+
 public class In {
 	
 	public static String dent(Object o) {
 		if (o == null)
 			return null;
+		// Put a tab at the start of every line
 		return Pattern.compile("^", Pattern.MULTILINE).matcher(o.toString()).replaceAll("\t");
 	}
 
